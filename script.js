62bffd12adceff0027ea6436f3733db720563a12
Canttellyou <akbar123jason@gmail.com>
2021-09-11 22:34:13 +0000

    Adding Full Project 🎉

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ document.querySelector(".info").style.marginTop = "2rem";
 const overlay = document.querySelector(".overlay");
 const edit = document.querySelector(".edit");
 const closeM = document.querySelector(".close-modal");
-
+const overlayOne = document.querySelector(".overlay-1");
 var toolbarOptions = [
   [{ size: ["small", false, "large", "huge"] }],
   ["bold", "italic", "underline", "strike"],
@@ -32,7 +32,30 @@ document.addEventListener("keydown", function (esc) {
     closeModal();
   }
 });
+document.addEventListener("keydown", function (esc) {
+  if (
+    esc.key === "Escape" &&
+    !document.querySelector(".menu-nav").classList.contains("hide")
+  ) {
+    document.querySelector(".menu-nav").classList.add("hide");
+  }
+});
+
 function myFunction(x) {
   x.classList.toggle("change");
   document.querySelector(".menu-nav").classList.toggle("hide");
+  overlayOne.classList.toggle("hide");
 }
+overlayOne.addEventListener("click", function () {
+  overlayOne.classList.add("hide");
+  document.querySelector(".menu-nav").classList.add("hide");
+  document.querySelector(".container").classList.remove("change");
+});
+document.addEventListener("keydown", function (esc) {
+  if (
+    esc.key === "Escape" &&
+    document.querySelector(".container").classList.contains("change")
+  ) {
+    document.querySelector(".container").classList.remove("change");
+  }
+});
